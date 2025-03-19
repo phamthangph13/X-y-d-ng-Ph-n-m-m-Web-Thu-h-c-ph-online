f156ea8dc462e1a1b1525364d96054dd5e186c6b
Pham Thang <winnieph13@gmail.com>
2025-03-20 01:51:43 +0700

    update

--- WebProject/obj/Debug/net9.0/WebProject.AssemblyInfo.cs
+++ WebProject/obj/Debug/net9.0/WebProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31aec4253d35388b67f2addf475c8219a9bc814b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a9f00a2235b0d7b40aaaa0edfbcda00c2ba7b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
